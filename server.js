f59283f02490cf933390316e35d7ac2cd6b4ee14
victor <victorfoliveira0407@gmail.com>
2025-09-01 09:02:12 -0300

    query pocoes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import dados from "./src/data/dados.js";
-const {bruxos} = dados;
+const {bruxos, varinhas, pocoes, animais} = dados;
 
 // Criar aplicação com Express e configurar para aceitar JSON
 const app = express();
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 
 // Query Parameters no Node.js - API de Hogwarts
 app.get('/bruxos', (req, res) => {
-    const { casa, ano, especialidade, nome } = req.query;
+    const {casa, ano, especialidade, nome} = req.query;
     let resultado = bruxos;
   
     if (casa) {
@@ -80,6 +80,45 @@ app.post("/bruxos", (req, res) => {
     });
 });
 
+app.get("/varinhas", (req, res) => {
+  const {material, nucleo, comprimento} = req.query;
+  let resultado = varinhas;
+
+  if (material) {
+    resultado = resultado.filter(b => b.material.toLowerCase().includes(material.toLowerCase()));
+  }
+
+  if (nucleo) {
+    resultado = resultado.filter(b => b.nucleo.toLowerCase().includes(nucleo.toLowerCase()));
+  }
+
+  if (comprimento) {
+    resultado = resultado.filter(b => b.comprimento.toLowerCase().includes(comprimento.toLowerCase()));
+  }
+
+  res.status(200).json({
+    total: resultado.length,
+    data: resultado
+  });
+});
+
+app.get("/pocoes", (req, res) => {
+  const {nome, efeito} = req.query;
+  let resultado = pocoes;
+
+  if (nome) {
+    resultado = resultado.filter(b => b.nome.toLowerCase().includes(nome.toLowerCase()));
+  }
+
+  if (efeito) {
+    resultado = resultado.filter(b => b.efeito.toLowerCase().includes(efeito.toLowerCase()));
+  }
+
+  res.status(200).json({
+    total: resultado.length,
+    data: resultado
+  });
+});
 
 // Iniciar servidor escutando na porta definida
 app.listen(serverPort, () => {
